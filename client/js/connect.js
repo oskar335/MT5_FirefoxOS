37cc633d5ac6b754823cacd902cfd3b7417fbe68
nouriel <an903136@etu.unice.fr>
2015-03-04 14:44:04 +0100

    Issue#23
    Ajout de traitement du cas de connexion reseau impossible a la page
    d'initialisation.

--- client/js/connect.js
+++ client/js/connect.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
+
+	// Verifier l'etat de la connexion au lancement
+	//
+	if (navigator.onLine) {
+	  	// Accepter l'entree des informations de connexion au serveur distant
+		$("#server").prop('disabled', false);
+		$("#port").prop('disabled', false);
+	} else {
+		// Empecher l'entree des informations de connexion au serveur distant
+		$("#server").prop('disabled', true);
+		$("#port").prop('disabled', true);
+	}
+
+	// Traiter l'evenement de la connexion (modifie par Nouriel, traitement du button vis a vis de l'etat du reseau)
+	//
 	$("#connect").submit(function(){
+
 		var valide;
-		var server = $("#server").val();
-		if(server != null && (server == "localhost" || server.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/))){
-			valide = true;
+
+		// Accepter s'il y a une connexion, refuser sinon
+		if (navigator.onLine) {
+			var server = $("#server").val();
+			if(server != null && (server == "localhost" || server.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/))){
+				valide = true;
+			}
+			else{
+				alert("Adresse IP non valide");
+			}
+			
+			var port = $("#port").val();
+			if(!(port !=null || port.match(/\d{2,4}/))){
+				alert("Port non valide");
+				valide = false;
+			}
+			
 		}
-		else{
-			alert("Adresse IP non valide");
-		}
-		
-		var port = $("#port").val();
-		if(!(port !=null || port.match(/\d{2,4}/))){
-			alert("Port non valide");
+		else {
 			valide = false;
+			alert("Pas de connexion possible");
 		}
+
 		if(valide){
 			var address= "http://"+server+":"+port;
 			localStorage.setItem('address', address);
@@ -21,12 +47,37 @@ $(document).ready(function() {
 		}else{
 			return false;
 		}
+
 	});
 
+	// Traiter l'evenement du click du mode hors ligne
+	//
 	$("#modeHorsConnexion").click(function(){
 
 		localStorage.setItem('address', "");
 		window.location = "player.html";
 
 	});
+
+	// --- Traitement d'evenements de validite de connexion
+	// ----------------------------------------------------
+
+	// --- Traitement de connexion reseau viable
+	// addEventListener
+	$(window).on("offline", function() {
+		
+		// Empecher l'entree des informations de connexion au serveur distant
+		$("#server").prop('disabled', true);
+		$("#port").prop('disabled', true);
+	});
+
+	// --- Traitement de connexion reseau non viable
+	// addEventListener
+	$(window).on("online", function() {
+		
+		// Accepter l'entree des informations de connexion au serveur distant
+		$("#server").prop('disabled', false);
+		$("#port").prop('disabled', false);
+	});
+
 })
