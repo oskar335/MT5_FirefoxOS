66003518d1985e1ad5fbb55974aea9e21fa8ad95
romainChalut <romain91540@msn.com>
2015-03-10 09:20:05 +0100

    ajout de bootstrap-switch

--- client/js/player.js
+++ client/js/player.js
@@ -803,7 +803,20 @@ var isLocal = false;
     // adjust track volumes dependinf on all mute/solo states
     currentSong.setTrackVolumesDependingOnMuteSoloStatus();
   }
+//###MENU###
 
+function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+});
 });
 
 
